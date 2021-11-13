6fe5f244b81b0044bf859f9094587524399213d4
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2021-11-14 00:51:18 +0300

    switch

--- works/js01/ss.js
+++ works/js01/ss.js
@@ -340,4 +340,31 @@
 // }else{
 //     console.log('no id');
 // }
+//-------------------------------------------------------------------------------
+//switch
+let day;
+switch(new Date().getDay()){
+    case 0:
+        day = 'Pazar';
+        break;
+    case 1:
+        day = 'Pazartesi';
+        break;
+    case 2:
+        day = 'Sali';
+        break;    
+    case 3 :
+        day = 'Carsamba';
+        break;
+    case 4:
+        day = 'Persembe'
+        break;
+    case 5:
+        day = 'Cuma'
+        break;
+    case 6:
+        day = 'Cumartesi'
+        break;    
+};
+console.log(`bugun gunlerden ${day}`); 
 
