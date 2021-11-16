865c2500c330d35e991b4dd44134ceb4f640360a
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2021-11-17 00:09:03 +0300

    if else tasks

--- works/js01/ss.js
+++ works/js01/ss.js
@@ -374,3 +374,18 @@ trafigeCikis.setHours(0,0,0,0);
 var trafiktekiMs = Date.now() - trafigeCikis.getTime();
 console.log(trafiktekiMs);
 
+var resault = prompt("Who's there");
+if (resault == 'cancel') {
+    console.log('cancelled');
+}else if (result =='Admin') {
+    var password = prompt('enter your password');
+    if (password =='cancel') {
+        console.log('cancelled');
+    }else if (password== '1234') {
+         console.log('welcome Admin');
+    }else{
+        console.log('wrong password');
+    }
+}else{
+    console.log('I do not know');
+}
