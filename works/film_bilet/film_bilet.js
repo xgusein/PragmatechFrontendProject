9cbb54572d5b37ecb1d5ece487af2d3bf06b1adb
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2021-12-05 23:14:36 +0300

    create cinema ticket system

---
+++ works/film_bilet/film_bilet.js
@@ -0,0 +1,6 @@
+const container =document.querySelector('.container');
+container.addEventListener('click',function(e){
+    if(e.target.classList.container('seat') && e.target.classList.container('reserved')){
+        console.log(e.target)
+    }
+})
