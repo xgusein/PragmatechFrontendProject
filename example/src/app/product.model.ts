b2afd3f623988ef7ca17583935926e201661209c
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-03-06 23:32:06 +0300

    Data binding methods

---
+++ example/src/app/product.model.ts
@@ -0,0 +1,10 @@
+export class Product {
+    constructor(
+        public id?: number,
+        public name?: string,
+        public describtion?: string,
+        public imageUrl?: string,
+        public price?: number
+    ){
+    }
+}
