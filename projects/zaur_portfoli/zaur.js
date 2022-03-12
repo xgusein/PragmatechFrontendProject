72a0a18b85d04dff7e0069d6f0ab3b2b5d5b90d4
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-03-12 20:56:47 +0300

    navbar section

--- projects/zaur_portfoli/zaur.js
+++ projects/zaur_portfoli/zaur.js
@@ -27,3 +27,13 @@ const cursor = document.querySelector('.cursor');
                 cursor.classList.remove("expand");
             }, 500)
         })
+//navbar effects
+document.querySelector('.btn').onclick = function (e) {
+    var menu = document.querySelector('.menu');
+    var btn = document.querySelector('.btn');
+
+    menu.classList.toggle('is-active');
+    btn.classList.toggle('is-active');
+
+    e.preventDefault();
+}
