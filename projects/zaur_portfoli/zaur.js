45a8b172980dac1b2781719a02c910b77f35775d
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-03-12 14:30:54 +0300

    cursor has been finished

--- projects/zaur_portfoli/zaur.js
+++ projects/zaur_portfoli/zaur.js
@@ -1,17 +1,29 @@
-const cursor = document.querySelector(".cursor");
+// const cursor = document.querySelector('.cursor');
 
 //follow cursor on mousemove
-document.addEventListener("mousemove", (e) => {
-    let x = e.pageX;
-    let y = e.pageY;
+// document.addEventListener("mousemove", e => {
+    // let x = e.pageX;
+    // let y = e.pageY;
 
-    cursor.style.top = y + "px";
-    cursor.style.left= x + "px";
-    cursor.style.display = "block";
-});
+    // cursor.style.top = y + "px";
+    // cursor.style.left= x + "px";
+    // cursor.style.display = "block";
+//     cursor.setAttribute("style","top: "+(e.pageY-10)+"px;left:"+(e.pageX -10)+"px;")
+// });
 
-//cursor effects on mouseout
+//     document.addEventListener("click", () => {
+//         cursor.classList.add("expand");
+//     })
+const cursor = document.querySelector('.cursor');
 
-document.addEventListener("mouseout", () => {
-    cursor.style.display = "none";
-})
+        document.addEventListener('mousemove', e => {
+            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+        })
+
+        document.addEventListener('click', () => {
+            cursor.classList.add("expand");
+
+            setTimeout(() => {
+                cursor.classList.remove("expand");
+            }, 500)
+        })
