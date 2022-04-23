fc5115df3ac080a02f3d934531afd14ec996be55
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-04-23 14:50:01 +0300

    Routing sehamsi

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { UsersComponent } from './users/users.component';
 import { CategoriesComponent } from './categories/categories.component';
-import { Routes } from '@angular/router';
+import { from } from 'rxjs';
+import { NotfoundComponent } from './notfound/notfound.component';
 const appRoutes: Routes= [
     {path:'', component: HomeComponent}, //localhost:4200
     {path:'home', component: HomeComponent},
     {path:'products',component: ProductsComponent},
     {path:'users',component: UsersComponent},
+    {path:'**',component: NotfoundComponent}
 ];
 
 @NgModule({
@@ -20,10 +23,12 @@ const appRoutes: Routes= [
     HomeComponent,
     ProductsComponent,
     UsersComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
