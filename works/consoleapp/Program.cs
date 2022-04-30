4394b13f9ff70795201a35c1e4c16def6c828573
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-04-30 22:56:53 +0300

    .netcore variable

--- works/consoleapp/Program.cs
+++ works/consoleapp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+
+    static void Main(string[] args)
+    {
+        //* variable for weight information (byte,short,int,long)
+        byte kilo = 90;
+
+        //*variable for plate information
+        byte plate = 81;
+        string plate2 = "81";
+
+        //*variable for vehicle mileage
+        int km = 10000;
+
+        //*variable for customer id information
+        int customerId = 1235581;
+        string CustomerId = "A451331";
+
+       //*Variable for information on whether a product is on sale or not
+       bool issale = true;
+       bool _issale = false;
+       //*variable for salary information (float,decimal,double)
+       decimal salary = 3000.1468m;
+       //* variable for name and surname
+       string name = "Huseyn";
+       string surname = "Ibadzade";
+       string namesurname = "Huseyn Ibadzade";
+       string fullname = name + " " + surname;
+       //*Variable for branch code information
+       char branch = "B";
+
+    }
+}
