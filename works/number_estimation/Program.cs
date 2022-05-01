7308da6bbf5739508cce4782ea2227b275db1f3f
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-05-02 02:02:07 +0300

    number_estimation

---
+++ works/number_estimation/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program {
+
+    static void Main(string[] args)
+    {
+        
+        //number estimation
+        //Finding a number between 1-100
+        //please rate
+
+        int held = (new Random()).Next(1,100);
+        int right = 5;
+        int number;
+        int raund = 0;
+        while (right>0)
+        {
+            tur++;
+            System.Console.WriteLine($"{raund}.number: ");
+            number = int.Pars(System.Console.ReadLine());
+            right--;
+
+            if (number == held){
+                System.Console.WriteLine("Congrulation {raund} .");
+                break;
+            }
+            else
+            {
+                if (right == 0)
+                    break;
+                if(held>number){
+                    System.Console.WriteLine("go up");
+                }
+                else
+                {
+                    System.Console.WriteLine("go down");
+                }
+            }
+        }
+        System.Console.WriteLine("Game is over");
+
+
+    }
+}
