5504eb310ab4ce11c2c403f789b87cebf4d7fc0c
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-05-08 01:34:35 +0300

    Quiz_application frist section was finished

--- works/c#/Quiz_application/Program.cs
+++ works/c#/Quiz_application/Program.cs
@@ -15,7 +15,10 @@ namespace ConsoleApp
         public string Text { get; set; }
         public string[] Choices { get; set; }
         public string Answer { get; set; }
-        public bool checkAnswer { get; set; }
+        public bool checkAnswer  (string answer) 
+        { 
+            return this.Answer.ToLower() == answer.ToLower();
+        }
     }
 
 
@@ -24,10 +27,25 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            
            var q1 = new Question("Which is the best programming language?", new string[]{"Python","C#","Java","C++"},"C#");
            var q2 = new Question("Which is the popular programming language?", new string[]{"C#","Python","Java","C++"},"C#");
            var q3 = new Question("Which is the top earing programming language?", new string[]{"C#","Python","Java","C++"},"C#");
+
+           var questions = new Question[]{q1,q2,q3};
+
+           foreach (var q in questions)
+           {
+               System.Console.WriteLine($"question {index}: {q.Text}");
+               index++;
+
+               foreach (var c in q.Choices)
+               {
+                   System.Console.WriteLine($"-c{}");
+               }
+               System.Console.WriteLine("Answer:");
+               var answerr = Console.ReadLine();
+               System.Console.WriteLine(q.checkAnswer(answerr));
+           }
 
         }
     }
