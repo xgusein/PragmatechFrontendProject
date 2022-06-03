0b078c25a189912b645107107b3aea70b01005fc
Hüseyn Ibadzadə <ibadzadehuseyn@gmail.com>
2022-06-03 22:42:14 +0300

    last

---
+++ react-google-analytics/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { renderButton, checkSignedIn } from "./utils";
+import Report from './report';
+
+function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const updateSignin = (signedIn) => { //(3)
+    setIsSignedIn(signedIn);
+    if (!signedIn) {
+      renderButton();
+    }
+  };
+
+  const init = () => { //(2)
+    checkSignedIn()
+      .then((signedIn) => {
+        updateSignin(signedIn);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    window.gapi.load("auth2", init); //(1)
+  });
+
+  return (
+    <div className="App">
+      {!isSignedIn ? (
+        <div id="signin-button"></div>
+      ) : (
+        <Report />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-google-analytics1/src/utils.js
@@ -0,0 +1,39 @@
+const initAuth = () => {
+    return window.gapi.auth2.init({
+      client_id: "314604452986-ahgqr2hhaus2dmree1ne34idpn1fuugv.apps.googleusercontent.com", //paste your client ID here
+      scope: "https://www.googleapis.com/auth/analytics.readonly",
+    });
+  };
+
+export const checkSignedIn = () => {
+return new Promise((resolve, reject) => {
+    initAuth() //calls the previous function
+    .then(() => {
+        const auth = window.gapi.auth2.getAuthInstance(); //returns the GoogleAuth object
+        resolve(auth.isSignedIn.get()); //returns whether the current user is currently signed in
+    })
+    .catch((error) => {
+        reject(error);
+    });
+});
+};
+
+export const renderButton = () => {
+    window.gapi.signin2.render("signin-button", {
+      scope: "profile email",
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: "dark",
+      onsuccess: onSuccess,
+      onfailure: onFailure,
+    });
+  };
+  
+  const onSuccess = (googleUser) => {
+    console.log("Logged in as: " + googleUser.getBasicProfile().getName());
+  };
+  
+  const onFailure = (error) => {
+    console.error(error);
+  };
